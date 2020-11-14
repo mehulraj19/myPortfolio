4348b2205b02d6da722cf4a7b00dfb05f875791f
Mehul Raj <68051895+mehulraj19@users.noreply.github.com>
2020-11-14 23:40:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function function1() {
+    window.open(
+        'https://github.com/mehulraj19/Image-Processing-Project',
+        '_blank' 
+      );
+  }
+
+function function2() {
+    window.open(
+        'https://github.com/mehulraj19/Data-Analytics-Project',
+        '_blank' 
+      );
+  }
+
+function function3() {
+    window.open(
+        'https://github.com/mehulraj19/Cats-vs-Dogs-CNN',
+        '_blank' 
+      );
+  }
+
+//   MehulRaj_Resume
